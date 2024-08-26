c771081860d83221f9efb97ec25f07c003ba518f
Rob Laughlin <waterfallkicks@gmail.com>
2024-08-25 19:55:27 -0700

    Add word wrapping for card components

--- js/index.js
+++ js/index.js
@@ -4,40 +4,16 @@ import { Card } from "./Card.js";
 
 const TEST_PARAMS = {
     test: true,
-    numBooks: 50,
+    numBooks: 5,
     titleCharRange: [50, 50],
     authorCharRange: [50, 50]
 };
 
 function populateLibrary(library) {
     let libraryContainer = document.querySelector('.libraryContainer');
-    // console.log(library.books);
+
     const books = library.books.map(Card);
     libraryContainer.innerHTML = books.join('');
-
-    // library.books.forEach(book => {
-    //     let card = document.createElement("div");
-    //     card.classList.add("card");
-
-    //     let title = document.createElement("p");
-    //     title.classList.add("title");
-    //     // title.appendChild(document.createElement("strong"))
-    //     title.innerText = book.title;
-
-    //     let author = document.createElement("p");
-    //     author.classList.add("author");
-    //     author.innerText = book.author;
-
-    //     let pages = document.createElement("p");
-    //     pages.classList.add("pages");
-    //     pages.innerText = book.pages;
-
-    //     card.appendChild(title);
-    //     card.appendChild(author);
-    //     card.appendChild(pages);
-
-    //     libraryContainer.appendChild(card);
-    // });
 };
 
 function init(testParams) {
