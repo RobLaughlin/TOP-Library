5a3d69cff6a6fa4da7b5ce40e9dde1015a5f9d0a
Rob Laughlin <waterfallkicks@gmail.com>
2024-09-02 13:23:32 -0700

    Refactor protype chain pattern into classes

--- js/Library.js
+++ js/Library.js
@@ -1,20 +1,24 @@
-export function Library(books=[]) {
-    this.books = Array.from(books);
-};
-
-Library.prototype.addBook = function(book) {
-    this.books.push(book);
-};
-
-Library.prototype.removeBook = function(idx) {
-    if (idx >= 0 && idx < this.books.length) {
-        this.books.splice(idx, 1);
+export class Library {
+    constructor(books=[]) {
+        this.books = Array.from(books);
     }
-};
 
-export function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-};
+    addBook(book) {
+        this.books.push(book);
+    }
+
+    removeBook(idx) {
+        if (idx >= 0 && idx < this.books.length) {
+            this.books.splice(idx, 1);
+        }
+    }
+}
+
+export class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+}
